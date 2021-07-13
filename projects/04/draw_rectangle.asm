09ab762783b5058ac4d6a7f7ea9e2a2ee9df738c
elcortez <phersant@protonmail.com>
2021-07-13 09:39:58 +0200

    draw rectangle with predefined width and chosable length

--- projects/04/draw_rectangle.asm
+++ projects/04/draw_rectangle.asm
@@ -1,15 +1,32 @@
+@R0
+D = M
+@rectangle_length
+M = D
+@SCREEN
+D = A
+@current_screen_value
+M = D
+(BEGIN_BLACKNESS_LOOP)
+@current_screen_value
+A = M
+M = -1
 @32
 D = A
-@iterator
+@thirty_two
 M = D
-
-@SCREEN
-M = -1
-D = A
-
-(BEGIN_LOOP)
-D = D + 1
-A = D
-M = -1
-@BEGIN_LOOP
+(BEGIN_ITERATION_LOOP)
+@current_screen_value
+M = M + 1
+@thirty_two
+M = M - 1
+D = M
+@BEGIN_ITERATION_LOOP
+D;JGT
+@rectangle_length
+M = M - 1
+D = M
+@BEGIN_BLACKNESS_LOOP
+D;JGT
+(END)
+@END
 0;JMP
