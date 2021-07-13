c8264f81d26cd7b28f4605f47cfb68baef8fae42
elcortez <phersant@protonmail.com>
2021-07-13 12:31:45 +0200

    refacto rectangle drawing

--- projects/04/draw_rectangle.asm
+++ projects/04/draw_rectangle.asm
@@ -1,10 +1,10 @@
-@R0
-D = M
-@rectangle_length
-M = D
 @SCREEN
 D = A
 @current_screen_value
+M = D
+@R0
+D = M
+@rectangle_length
 M = D
 (BEGIN_BLACKNESS_LOOP)
 @current_screen_value
@@ -12,16 +12,8 @@ A = M
 M = -1
 @32
 D = A
-@thirty_two
-M = D
-(BEGIN_ITERATION_LOOP)
 @current_screen_value
-M = M + 1
-@thirty_two
-M = M - 1
-D = M
-@BEGIN_ITERATION_LOOP
-D;JGT
+M = D + M
 @rectangle_length
 M = M - 1
 D = M
