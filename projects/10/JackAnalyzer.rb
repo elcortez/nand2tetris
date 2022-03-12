3658b3605dd7b178a04ea5118dfdb9970ac2c19f
elcortez <phersant@protonmail.com>
2022-03-12 18:18:40 +0100

    push tester inside analyzer

--- projects/10/JackAnalyzer.rb
+++ projects/10/JackAnalyzer.rb
@@ -92,3 +92,13 @@ File.open("#{path}/#{filename}.xml", "w") do |xml_file|
   end
   xml_file.puts('</tokens>')
 end
+
+
+test_file = File.open(ARGV[0]).each_line.to_a.map(&:strip)
+
+File.open(ARGV[0]).each_line.to_a.map(&:strip).each_with_index do |line, index|
+  next if line == test_file[index]
+  p "Discrepancy found on line #{index}"
+end
+
+p 'Test successful !'
